02e88061c71bf0334a3f804bac1c2f3f4b778b95
kyrrahnork <kyrrahnork@gmail.com>
2018-09-29 15:57:00 -0700

    adds splash and start screens
    created rootstack in app.js. created splash screen and start screen to give a starting place for the team.

--- App.js
+++ App.js
@@ -1,58 +1,37 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
+import { 
+  Text 
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import {
+  createStackNavigator
+} from 'react-navigation';
 
-type Props = {};
-export default class App extends Component<Props> {
+import SplashScreen from './android/app/src/Splash/Splash';
+
+import StartScreen from './android/app/src/Start/Start';
+
+//this suppresse the yellow box warning
+import { YellowBox } from 'react-native'
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated'])
+
+const RootStack = createStackNavigator(
+  {
+    Splash: SplashScreen,
+    Start: StartScreen  
+  },
+  {
+    initialRouteName: 'Splash'
+  }
+)
+
+export default class App extends Component {
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+      <RootStack />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+
---
+++ android/app/src/Splash/Splash.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    Image,
+    Button,
+    StyleSheet
+} from 'react-native';
+
+export default class BackgroundImage extends Component{
+    static navigationOptions={
+        header:null
+    };
+
+    render() {
+        const resizeMode = 'center';
+
+    return (
+    <View
+        style={{
+        flex: 1,
+        backgroundColor: '#eee',
+        }}
+    >
+        <View
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <Image
+            style={{
+              flex: 1,
+              resizeMode: 'cover',
+              height: null, 
+              width: null,
+            }}
+            source={require('../main/res/drawable/screen.png')}
+          />
+        </View>
+        <View
+          style={{
+            position: 'absolute',
+            bottom: '10%',
+            left: '10%',
+            width: '80%',
+            flex: 1,
+            backgroundColor: 'transparent',
+            justifyContent: 'center',
+          }}
+        >
+          <Button
+            title='press to begin'
+            onPress={()=> this.props.navigation.navigate('Start')}
+          >
+          </Button>
+        </View>
+    </View>
+    )
+    }
+}
+
+const styles = StyleSheet.create({
+    backgroundImage:{
+        flex:1,
+        width:null,
+        height:null,
+        resizeMode:'cover'
+    }
+});
